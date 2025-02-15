bd1cedbd214db39377a4cbfe1e283e6692a05180
Sea Grapes <natuloid@gmail.com>
2025-02-15 14:35:18 -0800

    I did a thing

--- main.js
+++ main.js
@@ -5,3 +5,33 @@ let graphemes = graphemes_raw.split(/[\r\n]+/)
 let phon_const = graphemes[0].split(',')
 let phon_vowel = graphemes[1].split(',')
 
+
+
+
+const rand = array => array[Math.floor(Math.random() * array.length)]
+
+function rand_phon() {
+  let bool = Math.random() < 0.5
+
+  return bool ? rand(phon_const) : rand(phon_vowel)
+}
+
+function rand_word(length) {
+  return Array.from({length}, e => rand_phon()).join('')
+}
+
+window.rand_word = rand_word
+
+function build(parent, type, call) {
+  const res = document.createElement(type)
+  parent.append(res)
+  if (call) call(res)
+  return res
+}
+
+const display = document.querySelector('#display')
+
+Array.from({length: 100}, e => rand_word(3)).forEach(word => {
+  let res = build(display, 'div')
+  res.textContent = word
+})
